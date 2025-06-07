340a49ad728cce7756314862180052b93cdee408
BioFreelancer <89655045+biofreelancer@users.noreply.github.com>
2025-06-06 19:01:39 -0600

    Create 06jun2025-panmx.R

---
+++ yt-lives/06jun2025-panmx.R
@@ -0,0 +1,13 @@
+####
+# Copia y pega este codigo para replicar el ejercicio de hoy
+
+# cargar paquetes
+pacman::p_load( "vroom" )
+
+pan_promedio.df <- vroom( file = "https://data.biofreelancer.com/panmx" )
+
+head( pan_promedio.df )
+
+#########
+
+# El resto del script que generemos durante el video lo vamos a poner aquí abajo después de la clase
