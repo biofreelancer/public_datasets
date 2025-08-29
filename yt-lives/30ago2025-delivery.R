3b15d0705a0784c6e5c8b906cd2aa487f202c76c
BioFreelancer <89655045+biofreelancer@users.noreply.github.com>
2025-08-29 02:40:09 -0600

    Update 30ago2025-delivery.R

--- yt-lives/30ago2025-delivery.R
+++ yt-lives/30ago2025-delivery.R
@@ -1,4 +1,11 @@
 ####
 # Copia y pega este codigo para replicar el ejercicio de hoy
 
+# instala pacman para trabajar mas facil
 install.packages( "pacman" )
+
+# cargar la data simulada
+pacman::p_load( "vroom" )
+
+delivery.df <- read.table( file = "https://data.biofreelancer.com/delivery",
+                           header = TRUE, sep = "\t" )
