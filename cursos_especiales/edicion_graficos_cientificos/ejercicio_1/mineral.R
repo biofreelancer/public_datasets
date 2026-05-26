8b710a93f0cfea84580bdd91574e06d8b340ff73
IAO <iaguilaror@gmail.com>
2026-05-26 04:52:52 -0600

    exe ok

---
+++ cursos_especiales/edicion_graficos_cientificos/ejercicio_1/mineral.R
@@ -0,0 +1,41 @@
+# cargar paquetes
+pacman::p_load( "vroom", "ggplot2", "tidyr",
+                "dplyr", "ggsci" )
+
+# cargar data
+minerales.df <- vroom( file = "minerales.tsv" ) %>% 
+  filter( mineral %in% c( 
+    # "Bauxite",
+    # "Aluminium",
+    "Copper",
+    "Zinc",
+    "Lead",
+    "Lithium",
+    "Antimony" ) )
+
+# graficar cambio en toneladas
+linea1.p <- ggplot( data = minerales.df,
+                    mapping = aes( x = anio,
+                                   y =  toneladas ,
+                                   group = mineral,
+                                   color = mineral,
+                                   label = toneladas ) ) +
+  geom_line( )
+
+# ver la version fea
+linea1.p
+
+# mejoramos un poco
+linea2.p <- linea1.p +
+  geom_point( size = 5 ) +
+  geom_text( ) +
+  scale_color_aaas( ) +
+  theme_classic( )
+
+linea2.p
+
+# guardar en svg
+ggsave( plot = linea2.p,
+        file = "minerales.pdf",
+        width = 10,
+        height = 7 )
